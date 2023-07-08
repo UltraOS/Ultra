71a6512bb835a4020c709ffb0626900331aa5347
Daniil Tatianin <99danilt@gmail.com>
2023-07-09 00:57:44 +0300

    kernel/entry: invoke earlycall initcalls on startup
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,6 +1,17 @@
 #include <boot/entry.h>
+#include <initcall.h>
 
-void entry()
+static void do_initcalls(initcall_t *begin, initcall_t *end)
 {
+    initcall_t *fn;
+
+    for (fn = begin; fn < end; fn++) {
+        (*fn)();
+    }
+}
+
+void entry(void)
+{
+    do_initcalls(initcalls_earlycon_begin, initcalls_earlycon_end);
     for (;;);
 }
