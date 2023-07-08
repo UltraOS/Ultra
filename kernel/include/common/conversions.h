131497124dcd7c17ea2fbdcb230f5d1c8aa42dc2
Daniil Tatianin <99danilt@gmail.com>
2023-07-09 00:27:54 +0300

    kernel/common: add str_to_{i/u}{8/16/32/64} conversion helpers
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/include/common/conversions.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <common/string_view.h>
+#include <common/types.h>
+
+#define STR_TO_N_DECL(bits)                                                                   \
+    bool str_to_i##bits##_with_base(struct string_view str, i##bits *res, unsigned int base); \
+    bool str_to_u##bits##_with_base(struct string_view str, u##bits *res, unsigned int base); \
+                                                                                              \
+    static inline bool str_to_i##bits(struct string_view str, i##bits*res)                    \
+    {                                                                                         \
+        return str_to_i##bits##_with_base(str, res, 0);                                       \
+    }                                                                                         \
+                                                                                              \
+    static inline bool str_to_u##bits(struct string_view str, u##bits *res)                   \
+    {                                                                                         \
+        return str_to_u##bits##_with_base(str, res, 0);                                       \
+    }
+
+STR_TO_N_DECL(8)
+STR_TO_N_DECL(16)
+STR_TO_N_DECL(32)
+STR_TO_N_DECL(64)
