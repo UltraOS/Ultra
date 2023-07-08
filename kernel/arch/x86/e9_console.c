894a1cd29ec1bed0b777d98db32061bf7e73ecdd
Daniil Tatianin <99danilt@gmail.com>
2023-07-09 01:02:52 +0300

    kernel/arch/x86: introduce a basic QEMU debugcon earlycon module
    This module registers a basic console, which sinks all messages into the
    QEMU/Bochs 0xE9 '-debugcon'.
    We currently hardcode it as COMPILED_IN for simplicity.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/arch/x86/e9_console.c
@@ -0,0 +1,52 @@
+#include <common/types.h>
+
+#include <module.h>
+#include <console.h>
+#include <hypervisor.h>
+#include <io.h>
+
+io_window e9_iow;
+
+static void e9_write(struct console *con, const char *str, size_t count)
+{
+    UNUSED(con);
+    iowrite8_many(e9_iow, 0, (const u8*)str, count);
+}
+
+static struct console e9_console = {
+    .name = "E9 debugcon",
+    .write = e9_write,
+};
+
+static error_t e9_console_init(void)
+{
+    error_t ret = EOK;
+
+    if (!in_hypervisor())
+        return ret;
+
+    ret = io_window_map_pio(&e9_iow, 0xE9, 1);
+    if (unlikely(ret))
+        return ret;
+
+    if (ioread8(e9_iow, 0) != 0xE9)
+        goto unmap;
+
+    ret = register_console(&e9_console);
+    if (unlikely(ret))
+        goto unmap;
+
+    return ret;
+
+unmap:
+    io_window_unmap(&e9_iow);
+    return ret;
+}
+INITCALL_EARLYCON(e9_console_init);
+
+static error_t e9_console_fini(void)
+{
+    unregister_console(&e9_console);
+    return EOK;
+}
+MODULE_FINI(e9_console_fini);
