0d5e11cd2c8ff42839518cbc9026b328921fedf1
Daniil Tatianin <99danilt@gmail.com>
2023-07-09 01:32:01 +0300

    kernel/entry: log build architecture, version & commit sha on boot
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/entry.c
+++ kernel/entry.c
@@ -1,5 +1,6 @@
 #include <boot/entry.h>
 #include <initcall.h>
+#include <log.h>
 
 static void do_initcalls(initcall_t *begin, initcall_t *end)
 {
@@ -13,5 +14,8 @@ static void do_initcalls(initcall_t *begin, initcall_t *end)
 void entry(void)
 {
     do_initcalls(initcalls_earlycon_begin, initcalls_earlycon_end);
+
+    infoln("Booting ultra kernel v0.1-%s for %s...",
+           ULTRA_GIT_SHA, ULTRA_ARCH_STRING);
     for (;;);
 }
