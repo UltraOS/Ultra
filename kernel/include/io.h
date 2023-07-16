b4f8c89fae138cb61a79caf7971406fe68671f71
8infy <99danilt@gmail.com>
2023-07-16 13:13:36 +0300

    io: add an _at postfix for io helpers that read/write at an offset
    ...and add additional helpers without the posifx that always read/write
    at offset 0.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/io.h
+++ kernel/include/io.h
@@ -10,14 +10,24 @@ error_t io_window_map_pio(io_window*, u16 base, u16 length);
 
 void io_window_unmap(io_window*);
 
-#define IO_FN_DECL(width)                                              \
-    u##width ioread##width(io_window iow, size_t offset);              \
-    void ioread##width##_many(io_window iow, size_t offset,            \
-                              u##width *buf, size_t count);            \
-                                                                       \
-    void iowrite##width(io_window iow, size_t offset, u##width value); \
-    void iowrite##width##_many(io_window iow, size_t offset,           \
-                               const u##width *buf, size_t count);
+#define IO_FN_DECL(width)                                                   \
+    u##width ioread##width##_at(io_window iow, size_t offset);              \
+    void ioread##width##_many(io_window iow, size_t offset,                 \
+                              u##width *buf, size_t count);                 \
+                                                                            \
+    static inline u##width ioread##width(io_window iow)                     \
+    {                                                                       \
+        return ioread##width##_at(iow, 0);                                  \
+    }                                                                       \
+                                                                            \
+    void iowrite##width##_at(io_window iow, size_t offset, u##width value); \
+    void iowrite##width##_many(io_window iow, size_t offset,                \
+                               const u##width *buf, size_t count);          \
+                                                                            \
+    static inline void iowrite##width(io_window iow, u##width value)        \
+    {                                                                       \
+        iowrite##width##_at(iow, 0, value);                                 \
+    }
 
 IO_FN_DECL(8)
 IO_FN_DECL(16)
