6063f44b7d10c537a8a104cae3baa843481ded0b
Daniil Tatianin <99danilt@gmail.com>
2025-06-22 15:33:33 +0300

    panic: add a vpanic function
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -2,8 +2,18 @@
 
 #include <panic.h>
 
-void panic(const char *reason, ...)
+void vpanic(const char *reason, va_list vlist)
 {
     UNREFERENCED_PARAMETER(reason);
+    UNREFERENCED_PARAMETER(vlist);
     for (;;);
+}
+
+void panic(const char *reason, ...)
+{
+    va_list vlist;
+
+    va_start(vlist, reason);
+    vpanic(reason, vlist);
+    va_end(vlist);
 }
