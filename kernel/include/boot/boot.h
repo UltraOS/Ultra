7673ec78e5993e72a2582904316884cbeca346d6
Daniil Tatianin <99danilt@gmail.com>
2025-06-22 16:16:29 +0300

    boot: introduce boot_context & make entry() use it
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/boot/boot.h
+++ kernel/include/boot/boot.h
@@ -1,1 +1,19 @@
-void entry();
+#pragma once
+
+#include <boot/ultra_protocol.h>
+#include <common/types.h>
+
+struct boot_context {
+    struct ultra_platform_info_attribute *platform_info;
+    struct ultra_kernel_info_attribute *kernel_info;
+    struct ultra_memory_map_attribute *memory_map;
+    struct ultra_command_line_attribute *cmdline;
+    struct ultra_framebuffer_attribute *fb;
+
+    struct ultra_module_info_attribute *modules;
+    size_t num_modules;
+};
+
+extern struct boot_context g_boot_ctx;
+
+void entry(struct ultra_boot_context *ctx);
