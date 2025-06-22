a0f0c714c811c7182ae33ce3728d3de3a6be14ff
Daniil Tatianin <99danilt@gmail.com>
2025-06-22 16:25:24 +0300

    bug: introduce WARN_ON{_WITH_MSG}
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/bug.h
+++ kernel/include/bug.h
@@ -2,11 +2,67 @@
 
 #include <common/attributes.h>
 #include <panic.h>
+#include <log.h>
 
-#define BUG() panic("BUG! At %s() in file %s:%d\n", __func__, __FILE__, __LINE__)
+#define BUG() \
+    panic("BUG! At %s() in file %s:%d\n", __func__, __FILE__, __LINE__)
+
+#define BUG_WITH_MSG(msg, ...) \
+    panic("BUG! " msg, ##__VA_ARGS__)
 
 #define BUG_ON(expr)        \
     do {                    \
         if (unlikely(expr)) \
             BUG();          \
     } while (0)
+
+#define BUG_ON_WITH_MSG(expr, msg, ...)     \
+    do {                                    \
+        if (unlikely(expr))                 \
+            BUG_WITH_MSG(msg, __VA_ARGS__); \
+    } while (0)
+
+#define WARN()                                            \
+    pr_warn("WARNING: At %s() in file %s:%d\n", __func__, \
+            __FILE__, __LINE__)
+
+#ifndef _MSC_VER
+
+#define WARN_ON_WITH_MSG(expr, msg, ...) ({ \
+    bool true_cond = !!((expr));            \
+    if (unlikely(true_cond))                \
+        pr_warn(msg, __VA_ARGS__);          \
+    unlikely(true_cond);                    \
+})
+
+#define WARN_ON(expr) ({         \
+    bool true_cond = !!((expr)); \
+    if (unlikely(true_cond))     \
+        WARN();                  \
+    unlikely(true_cond);         \
+})
+
+#else
+
+#ifndef ULTRA_TEST
+#error MSVC is only supported in test mode
+#endif
+
+static inline bool WARN_ON_WITH_MSG(bool expr, const char *msg, ...)
+{
+    if (expr) {
+        va_list vlist;
+
+        va_start(vlist, msg);
+        vprint(msg, vlist);
+        va_end(vlist);
+    }
+
+    return expr;
+}
+
+#define WARN_ON(expr)                                                    \
+    WARN_ON_WITH_MSG(expr, "WARNING: At %s() in file %s:%d\n", __func__, \
+                     __FILE__, __LINE__)
+
+#endif
