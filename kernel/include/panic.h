0dd67549003955357bc928c24e53055d55e6cb20
Daniil Tatianin <99danilt@gmail.com>
2025-06-22 15:33:33 +0300

    panic: add a vpanic function
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/panic.h
+++ kernel/include/panic.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <stdarg.h>
 #include <common/attributes.h>
 
+NORETURN
+void vpanic(const char *reason, va_list);
+
+NORETURN
 PRINTF_DECL(1, 2)
 void panic(const char *reason, ...);
