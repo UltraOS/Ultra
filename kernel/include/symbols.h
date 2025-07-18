2ae2b71054dbe9ef6de70afd3ff0b906d0e8d936
Daniil Tatianin <99danilt@gmail.com>
2025-07-18 13:31:26 +0300

    Always refer to linker symbols using LINKER_SYMBOL
    1. So that the user doesn't have to always type out the whole thing
    2. Shorter
    3. Gets the point across
    4. Harder to make a mistake
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/symbols.h
+++ kernel/include/symbols.h
@@ -1,22 +1,24 @@
 #include <common/types.h>
 #include <common/error.h>
 
-extern u8 g_linker_symbol_text_begin[];
-extern u8 g_linker_symbol_text_end[];
+#include <linker.h>
 
-extern u8 g_linker_symbol_eh_frame_hdr_begin[];
-extern u8 g_linker_symbol_eh_frame_hdr_end[];
+extern u8 LINKER_SYMBOL(text_begin)[];
+extern u8 LINKER_SYMBOL(text_end)[];
 
-extern u8 g_linker_symbol_eh_frame_begin[];
-extern u8 g_linker_symbol_eh_frame_end[];
+extern u8 LINKER_SYMBOL(eh_frame_hdr_begin)[];
+extern u8 LINKER_SYMBOL(eh_frame_hdr_end)[];
+
+extern u8 LINKER_SYMBOL(eh_frame_begin)[];
+extern u8 LINKER_SYMBOL(eh_frame_end)[];
 
 // 127 chars & a NULL terminator
 #define MAX_SYMBOL_LENGTH (127 + 1)
 
 static inline bool address_is_kernel_code(ptr_t address)
 {
-    return address >= (ptr_t)g_linker_symbol_text_begin &&
-           address <= (ptr_t)g_linker_symbol_text_end;
+    return address >= (ptr_t)LINKER_SYMBOL(text_begin) &&
+           address <= (ptr_t)LINKER_SYMBOL(text_end);
 }
 
 error_t symbol_lookup_by_address(
