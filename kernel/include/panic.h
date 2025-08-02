78f1b42a95a258fa19883f3284e18e79d691a956
Daniil Tatianin <99danilt@gmail.com>
2025-08-02 21:41:22 +0300

    panic: get rid of vpanic & dump the call stack when panicking
    This also adds a guard to make sure we don't panic recursively.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/panic.h
+++ kernel/include/panic.h
@@ -4,8 +4,5 @@
 #include <common/attributes.h>
 
 NORETURN
-void vpanic(const char *reason, va_list);
-
-NORETURN
 PRINTF_DECL(1, 2)
 void panic(const char *reason, ...);
--- kernel/panic.c
+++ kernel/panic.c
@@ -1,19 +1,28 @@
 #include <common/helpers.h>
+#include <common/types.h>
+#include <common/atomic.h>
+#include <common/format.h>
 
 #include <panic.h>
+#include <log.h>
 
-void vpanic(const char *reason, va_list vlist)
-{
-    UNREFERENCED_PARAMETER(reason);
-    UNREFERENCED_PARAMETER(vlist);
-    for (;;);
-}
+static bool g_in_panic;
 
 void panic(const char *reason, ...)
 {
+    static char panic_buf[256];
     va_list vlist;
 
+    if (atomic_xchg(&g_in_panic, true, MO_ACQ_REL))
+        goto hang;
+
     va_start(vlist, reason);
-    vpanic(reason, vlist);
+    vsnprintf(panic_buf, sizeof(panic_buf), reason, vlist);
     va_end(vlist);
+
+    pr_emerg("Kernel panic: %s", panic_buf);
+    dump_stack(LOG_LEVEL_EMERG, NULL);
+
+hang:
+    for (;;);
 }
