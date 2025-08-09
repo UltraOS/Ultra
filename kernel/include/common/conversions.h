92acdb55de00fc95edc1af58750b8e764edc6b60
Daniil Tatianin <99danilt@gmail.com>
2025-08-09 17:46:29 +0300

    common/conversions: use error_t instead of bool
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/common/conversions.h
+++ kernel/include/common/conversions.h
@@ -2,23 +2,24 @@
 
 #include <common/string_container.h>
 #include <common/types.h>
+#include <common/error.h>
 
-#define STR_TO_N_DECL(bits)                                            \
-    bool str_to_i##bits##_with_base(                                   \
-        struct string str, i##bits *res, unsigned int base             \
-    );                                                                 \
-    bool str_to_u##bits##_with_base(                                   \
-        struct string str, u##bits *res, unsigned int base             \
-    );                                                                 \
-                                                                       \
-    static inline bool str_to_i##bits(struct string str, i##bits*res)  \
-    {                                                                  \
-        return str_to_i##bits##_with_base(str, res, 0);                \
-    }                                                                  \
-                                                                       \
-    static inline bool str_to_u##bits(struct string str, u##bits *res) \
-    {                                                                  \
-        return str_to_u##bits##_with_base(str, res, 0);                \
+#define STR_TO_N_DECL(bits)                                               \
+    error_t str_to_i##bits##_with_base(                                   \
+        struct string str, i##bits *res, unsigned int base                \
+    );                                                                    \
+    error_t str_to_u##bits##_with_base(                                   \
+        struct string str, u##bits *res, unsigned int base                \
+    );                                                                    \
+                                                                          \
+    static inline error_t str_to_i##bits(struct string str, i##bits*res)  \
+    {                                                                     \
+        return str_to_i##bits##_with_base(str, res, 0);                   \
+    }                                                                     \
+                                                                          \
+    static inline error_t str_to_u##bits(struct string str, u##bits *res) \
+    {                                                                     \
+        return str_to_u##bits##_with_base(str, res, 0);                   \
     }
 
 STR_TO_N_DECL(8)
