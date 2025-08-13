8db09924fd81b2f4e1cc58042c1d1e1592910df9
Daniil Tatianin <99danilt@gmail.com>
2025-08-13 15:23:32 +0300

    kernel/linker: add macros for referencing special section arrays
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/linker.h
+++ kernel/include/linker.h
@@ -3,3 +3,10 @@
 #include <common/helpers.h>
 
 #define LINKER_SYMBOL(x) CONCAT(g_linker_symbol_, x)
+
+#define SECTION_ARRAY_BEGIN(x) CONCAT(LINKER_SYMBOL(x), _begin)
+#define SECTION_ARRAY_END(x) CONCAT(LINKER_SYMBOL(x), _end)
+#define SECTION_ARRAY_SIZE(x) (SECTION_ARRAY_END(x) - SECTION_ARRAY_BEGIN(x))
+
+#define SECTION_ARRAY_ARGS(x) \
+    SECTION_ARRAY_BEGIN(x), SECTION_ARRAY_SIZE(x)
