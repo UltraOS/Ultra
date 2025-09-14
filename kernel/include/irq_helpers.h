e54b510794963f8a0b7042d2bfdae0630ed632a3
Daniil Tatianin <99danilt@gmail.com>
2025-09-14 13:14:11 +0300

    Introduce IRQ state helpers
    These helpers have a generic header, as well as architecture-specific
    implementation details expected to be in arch/private.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/include/irq_helpers.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <common/types.h>
+#include <common/attributes.h>
+
+#include <arch/private/irq_helpers.h>
+
+static ALWAYS_INLINE bool irq_enabled(void)
+{
+    return arch_irq_is_enabled_state(arch_irq_state());
+}
+
+/*
+ * Save the arch-specific IRQ state and disable IRQs if they were previously
+ * enabled.
+ */
+static ALWAYS_INLINE irq_state_t irq_state_save(void)
+{
+    irq_state_t state;
+
+    state = arch_irq_state();
+    if (arch_irq_is_enabled_state(state))
+        arch_irq_disable();
+
+    return state;
+}
+
+// Restore the IRQ state from the arch-specific state saved earlier
+static ALWAYS_INLINE void irq_state_restore(irq_state_t state)
+{
+    if (!arch_irq_is_enabled_state(state))
+        return;
+
+    arch_irq_enable();
+}
