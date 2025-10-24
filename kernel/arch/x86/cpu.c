b57d2db693a5acc1733b02f90f0fb6bb10f48cd8
Daniil Tatianin <99danilt@gmail.com>
2025-10-24 13:15:34 +0300

    cpu: introduce cpuid_subleaf
    Some CPUID tests require ECX != 0
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/arch/x86/cpu.c
+++ kernel/arch/x86/cpu.c
@@ -1,8 +1,14 @@
 #include <arch/private/cpu.h>
 
 void cpuid(u32 function, struct cpuid_res *id)
+void cpuid_subleaf(u32 function, u32 subleaf, struct cpuid_res *id)
 {
     asm volatile("cpuid"
             : "=a"(id->a), "=b"(id->b), "=c"(id->c), "=d"(id->d)
-            : "a"(function), "c"(0));
+            : "a"(function), "c"(subleaf));
+}
+
+void cpuid(u32 function, struct cpuid_res *id)
+{
+    cpuid_subleaf(function, 0, id);
 }
