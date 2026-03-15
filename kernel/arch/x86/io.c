0964612dff6b52538895f63bc1a4150eab94361a
Daniil Tatianin <99danilt@gmail.com>
2026-03-15 15:19:56 +0300

    x86/io: use phys_to_virt for mapping memory
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/arch/x86/io.c
+++ kernel/arch/x86/io.c
@@ -1,14 +1,18 @@
+#include <private/arch/io.h>
+
 #include <common/types.h>
 #include <common/error.h>
+
 #include <bug.h>
 #include <switch.h>
-#include <private/arch/io.h>
+#include <io.h>
 
 ptr_or_error_t arch_map_memory_io(phys_addr_t phys_base, size_t length)
 {
-    UNREFERENCED_PARAMETER(phys_base);
     UNREFERENCED_PARAMETER(length);
-    return encode_error_ptr(ENOTSUP);
+
+    // FIXME: this should use actual map helpers
+    return phys_to_virt(phys_base);
 }
 
 pio_addr_t arch_map_port_io(phys_addr_t phys_base, size_t length)
