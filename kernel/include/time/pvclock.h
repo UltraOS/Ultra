bec33c5c38cd14a34f23ce21db6b3914a36cd37b
Daniil Tatianin <99danilt@gmail.com>
2026-05-01 03:09:08 +0300

    Add basic pvclock support
    pvclock is a paravirtualized device that provides a formula to scale
    guest TSC to nanoseconds, it is supported by Xen and KVM.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/include/time/pvclock.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <common/attributes.h>
+#include <common/types.h>
+#include <common/bit.h>
+
+#include <per_cpu_decls.h>
+
+struct pvclock_vcpu_time_info {
+    u32 version;
+    u32 rsvd0;
+    u64 host_tsc_timestamp;
+    u64 host_ns_since_boot;
+    u32 guest_tsc_mul;
+    i8 guest_tsc_shift;
+
+#define PVCLOCK_TSC_STABLE BIT_U8(0)
+#define PVCLOCK_GUEST_STOPPED BIT_U8(1)
+    u8 flags;
+
+    u8 rsvd[2];
+};
+EXPECT_SIZEOF(struct pvclock_vcpu_time_info, 32);
+
+struct pvclock_wall_clock {
+    u32 version;
+    u32 sec;
+    u32 nsec;
+};
+EXPECT_SIZEOF(struct pvclock_wall_clock, 12);
+
+DECLARE_PER_CPU(struct pvclock_vcpu_time_info*, g_this_cpu_time_info);
+
+void pvclock_enable_stable_bit(void);
+
+u64 pvclock_read_from(struct pvclock_vcpu_time_info*);
+#define pvclock_read() pvclock_read_from(this_cpu_read(g_this_cpu_time_info))
