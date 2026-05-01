07a62c644c36d2cbc2fea06b91ced772d8819006
Daniil Tatianin <99danilt@gmail.com>
2026-05-01 03:09:08 +0300

    Add basic pvclock support
    pvclock is a paravirtualized device that provides a formula to scale
    guest TSC to nanoseconds, it is supported by Xen and KVM.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/drivers/time/pvclock.c
@@ -0,0 +1,90 @@
+#include <arch/private/cpu.h>
+
+#include <time/pvclock.h>
+
+static bool s_has_stable_bit;
+
+DEFINE_PER_CPU(struct pvclock_vcpu_time_info*, g_this_cpu_time_info);
+
+void pvclock_enable_stable_bit(void)
+{
+    s_has_stable_bit = true;
+}
+
+static u32 pvclock_read_start(struct pvclock_vcpu_time_info *info)
+{
+    u32 version;
+
+    do {
+        version = atomic_load_relaxed(&info->version);
+    } while (version & 1);
+
+    barrier_acquire();
+    return version;
+}
+
+static u64 pvclock_cycles_to_ns(struct pvclock_vcpu_time_info *info, u64 tsc)
+{
+    u64 host_ns, elapsed_ticks, elapsed_ns;
+    i8 shift;
+    u32 mul;
+
+    elapsed_ticks = tsc - atomic_load_relaxed(&info->host_tsc_timestamp);
+
+    shift = atomic_load_relaxed(&info->guest_tsc_shift);
+    if (shift < 0)
+        elapsed_ticks >>= -shift;
+    else
+        elapsed_ticks <<= shift;
+
+    mul = atomic_load_relaxed(&info->guest_tsc_mul);
+    elapsed_ns = ((u128)elapsed_ticks * mul) >> 32;
+
+    host_ns = atomic_load_relaxed(&info->host_ns_since_boot);
+
+    barrier_acquire();
+    return host_ns + elapsed_ns;
+}
+
+static u64 pvclock_read_stablize(u64 this_read)
+{
+    static u64 s_last_read_ns;
+    u64 last_read;
+
+    last_read = atomic_load_relaxed(&s_last_read_ns);
+    do {
+        /*
+         * The value we have read is behind a read some other vCPU has
+         * performed before us. Ignore whatever we read and return that
+         * instead.
+         */
+        if (this_read <= last_read)
+            return last_read;
+    } while (!atomic_cmpxchg_explicit(&s_last_read_ns, &last_read, this_read,
+                                      MO_RELAXED, MO_RELAXED));
+    return this_read;
+}
+
+u64 pvclock_read_from(struct pvclock_vcpu_time_info *info)
+{
+    u32 version;
+    u64 ns_since_boot;
+    u8 flags;
+
+    do {
+        version = pvclock_read_start(info);
+        flags = atomic_load_relaxed(&info->flags);
+        ns_since_boot = pvclock_cycles_to_ns(info, rdtsc());
+    } while (atomic_load_relaxed(&info->version) != version);
+
+    if (s_has_stable_bit && (flags & PVCLOCK_TSC_STABLE))
+        // Stable pvclock, no further validation needed
+        return ns_since_boot;
+
+    /*
+     * pvclock is not stable, so we aren't guaranteed to see synchornized
+     * progress on all vCPUs. Debounce this read against a global monotonic
+     * ns counter so we dont warp ourselves into the past.
+     */
+    return pvclock_read_stablize(ns_since_boot);
+}
