c03bf561f7babf76446e7129116945ed35a75e65
Daniil Tatianin <99danilt@gmail.com>
2026-05-04 23:10:28 +0300

    unwind: make it possible to unwind bogus function pointer calls
    This adds a new arch-specific arch_try_recover_previous_frame callback,
    which is a best effort attempt to recover the caller frame.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/arch/x86_64/unwind.c
+++ kernel/arch/x86_64/unwind.c
@@ -1,6 +1,9 @@
 #include <private/arch/unwind.h>
 #include <arch/private/unwind.h>
+
+#include <symbols.h>
 #include <unwind.h>
+#include <unsafe_access.h>
 
 void arch_registers_to_dwarf_registers(
     struct registers *regs, ptr_t *dwarf_regs
@@ -23,4 +26,38 @@ void arch_registers_to_dwarf_registers(
     dwarf_regs[DWARF_REG_R14] = regs->r14;
     dwarf_regs[DWARF_REG_R15] = regs->r15;
     dwarf_regs[DWARF_REG_RIP] = regs->rip;
+}
+
+error_t arch_try_recover_previous_frame(struct unwind_state *state) {
+    error_t ret;
+    reg_t prev_pc;
+    reg_t *rsp = (reg_t*)state->frame[DWARF_REG_RSP];
+
+    /*
+     * The address of our caller should be right there, at the current RSP.
+     * If this was indeed a bogus function pointer call, we should be able
+     * to recover it no problem.
+     */
+    ret = try_memcpy(&prev_pc, rsp, sizeof(reg_t));
+    if (ret != EOK)
+        return ret;
+
+    if (!address_is_kernel_code(prev_pc))
+        /*
+         * Yeah, no.. There's a bigger stack corruption going on.
+         * Whatever we have recovered from our stack is a bogus value as well.
+         */
+        return ENOENT;
+
+    /*
+     * Success, now fix up the frame and attempt to fast forward it to
+     * the after-the-call state. Basically emulate a 'ret' instruction
+     * that pops the RIP off the stack and jumps to it.
+     */
+    state->signal_frame = false;
+    state->ret_reg_idx = ARCH_DWARF_PC_REG;
+    state->frame[state->ret_reg_idx] = prev_pc;
+    state->frame[DWARF_REG_RSP] += sizeof(reg_t);
+
+    return EOK;
 }
