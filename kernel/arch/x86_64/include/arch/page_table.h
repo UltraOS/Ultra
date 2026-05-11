3f807645beb98aa94155c230512266b5aac8b3fa
Daniil Tatianin <99danilt@gmail.com>
2026-05-11 14:15:33 +0300

    Rework the page table abstractions & x86_64 impl to make them usable
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/arch/x86_64/include/arch/page_table.h
+++ kernel/arch/x86_64/include/arch/page_table.h
@@ -1,23 +1,35 @@
 #pragma once
 
 #include <common/types.h>
+#include <common/bit.h>
+
 #include <arch/constants.h>
 #include <memory/vm_flags.h>
 #include <io.h>
 
 #define X86_MAX_PHYS_BITS 52
-#define X86_PHYS_MASK ((1ull << X86_MAX_PHYS_BITS) - 1)
+#define X86_PHYS_MASK MAKE_BIT_MASK(X86_MAX_PHYS_BITS - 1, 0)
 
-#define X86_PT_PRESENT (1ull << 0)
-#define X86_PT_WRITE (1ull << 1)
-#define X86_PT_USER (1ull << 2)
-#define X86_PT_ACCESSED (1ull << 5)
-#define X86_PT_DIRTY (1ull << 6)
-#define X86_PT_HUGE (1ull << 7)
-#define X86_PT_GLOBAL (1ull << 8)
-#define X86_PT_NX (1ull << 63)
+#define X86_PT_PRESENT BIT(0)
+#define X86_PT_WRITE BIT(1)
+#define X86_PT_USER BIT(2)
+#define X86_PT_WRITETHROUGH BIT(3)
+#define X86_PT_UNCACHED BIT(4)
+#define X86_PT_ACCESSED BIT(5)
+#define X86_PT_DIRTY BIT(6)
+#define X86_PT_HUGE BIT(7)
+#define X86_PT_GLOBAL BIT(8)
+#define X86_PT_NX BIT(63)
 
-#define X86_PT_MASK (X86_PT_PRESENT | X86_PT_WRITE | X86_PT_USER)
+#define X86_SMALL_PAT_SHIFT 7
+#define X86_HUGE_PAT_SHIFT 12
+#define X86_PT_SMALL_PAT BIT(X86_SMALL_PAT_SHIFT)
+#define X86_PT_HUGE_PAT BIT(X86_HUGE_PAT_SHIFT)
+
+#define MAKE_X86_PT_TYPE(idx) struct pt##idx { u64 value; };
+
+// Raw arch-specific page table entry flags
+typedef struct pt_prot { u64 value; } pt_prot;
 
 /*
  * Intel® Xeon Phi™ Processor x200 Product Family (KNL4):
@@ -31,41 +43,57 @@
  */
 #define X86_KNL4_ERRATUM_MASK (X86_PT_ACCESSED | X86_PT_DIRTY)
 
-#define MAKE_X86_PT_HELPERS(idx)                                    \
-    static inline bool pt##idx##_present(struct pt##idx *pt)        \
-    {                                                               \
-        return pt->value & X86_PT_PRESENT;                          \
-    }                                                               \
-                                                                    \
-    static inline void *pt##idx##_to_virt(struct pt##idx *pt)       \
-    {                                                               \
-        phys_addr_t phys_addr;                                      \
-                                                                    \
-        phys_addr = pt->value & CONCAT(CONCAT(PT, idx), _PFN_MASK); \
-        return phys_to_virt(phys_addr);                             \
-    }                                                               \
-    static inline bool pt##idx##_none(struct pt##idx *pt)           \
-    {                                                               \
-        return (pt->value & ~X86_KNL4_ERRATUM_MASK) == 0;           \
+#define MAKE_X86_PT_TO_VIRT(idx)                              \
+                                                              \
+    static inline void *pt##idx##_to_virt(struct pt##idx *pt) \
+    {                                                         \
+        phys_addr_t phys_addr;                                \
+                                                              \
+        phys_addr = pt->value & X86_PAGE_MASK;                \
+        return phys_to_virt(phys_addr);                       \
     }
 
-#define MAKE_X86_PT_POPULATE(idx, idx_minus_one)                    \
-    static inline void pt##idx##_populate(                          \
-        struct pt##idx *parent, struct pt##idx_minus_one *child)    \
-    {                                                               \
-        parent->value = virt_to_phys(child) | X86_PT_MASK;          \
+#define MAKE_X86_PT_HELPERS(idx)                             \
+    static inline bool pt##idx##_present(struct pt##idx *pt) \
+    {                                                        \
+        return pt->value & X86_PT_PRESENT;                   \
+    }                                                        \
+                                                             \
+    static inline bool pt##idx##_none(struct pt##idx *pt)    \
+    {                                                        \
+        return (pt->value & ~X86_KNL4_ERRATUM_MASK) == 0;    \
+    }                                                        \
+                                                             \
+    static inline void pt##idx##_clear(struct pt##idx *pt)   \
+    {                                                        \
+        pt->value = 0;                                       \
     }
 
+// Mask that is used for all intermediate page-table levels
+#define X86_PT_MASK (X86_PT_PRESENT | X86_PT_WRITE | X86_PT_USER)
 
-struct pt_prot { u64 value; };
-struct pt_prot pt_prot_from_vm_prot(enum vm_prot);
+#define MAKE_X86_PT_POPULATE(idx, idx_minus_one)                 \
+    static inline void pt##idx##_populate(                       \
+        struct pt##idx *parent, struct pt##idx_minus_one *child) \
+    {                                                            \
+        parent->value = virt_to_phys(child) | X86_PT_MASK;       \
+    }
 
-#define MAKE_X86_PT_TYPE(idx) struct pt##idx { u64 value; };
+#define MAKE_X86_PT_LEAF_HELPERS(idx, can_be_leaf_ret)       \
+    static inline bool pt##idx##_can_be_leaf(void)           \
+    {                                                        \
+        return can_be_leaf_ret;                              \
+    }                                                        \
+                                                             \
+    static inline bool pt##idx##_is_leaf(struct pt##idx *pt) \
+    {                                                        \
+        return pt->value & X86_PT_HUGE;                      \
+    }
 
 MAKE_X86_PT_TYPE(1)
 
 static inline void pt1_populate(
-    struct pt1 *parent, phys_addr_t phys_addr, struct pt_prot prot
+    struct pt1 *parent, phys_addr_t phys_addr, pt_prot prot
 )
 {
     parent->value = phys_addr | prot.value;
@@ -105,15 +133,9 @@ extern u64 g_pt4_num_entries;
 #define PT5_SHIFT g_pt5_shift
 #define PT5_NUM_ENTRIES X86_PT_LVL_ENTRIES
 
-#define X86_PAGE_MASK (X86_PHYS_MASK & (~(PAGE_SIZE - 1ull)))
-#define X86_PT3_MASK (X86_PHYS_MASK & (~((1ull << PT3_SHIFT) - 1)))
-#define X86_PT2_MASK (X86_PHYS_MASK & (~((1ull << PT2_SHIFT) - 1)))
-
-#define PT5_PFN_MASK X86_PAGE_MASK
-#define PT4_PFN_MASK X86_PAGE_MASK
-#define PT3_PFN_MASK X86_PT3_MASK
-#define PT2_PFN_MASK X86_PT2_MASK
-#define PT1_PFN_MASK X86_PAGE_MASK
+#define X86_PAGE_MASK MAKE_BIT_MASK(X86_MAX_PHYS_BITS - 1, PAGE_SHIFT)
+#define X86_PT3_MASK  MAKE_BIT_MASK(X86_MAX_PHYS_BITS - 1, PT3_SHIFT)
+#define X86_PT2_MASK  MAKE_BIT_MASK(X86_MAX_PHYS_BITS - 1, PT2_SHIFT)
 
 static inline void pt5_populate(struct pt5 *parent, struct pt4 *child)
 {
@@ -143,14 +165,76 @@ static inline void *pt5_to_virt(struct pt5 *pt)
 {
     phys_addr_t phys_addr;
 
-    phys_addr = pt->value & PT5_PFN_MASK;
+    phys_addr = pt->value & X86_PAGE_MASK;
     return phys_to_virt(phys_addr);
 }
 
 MAKE_X86_PT_HELPERS(4)
+MAKE_X86_PT_TO_VIRT(4)
+
 MAKE_X86_PT_HELPERS(3)
+
+extern bool g_have_gb_pages;
+
+#define ARCH_HAS_CUSTOM_PT3_LEAF
+MAKE_X86_PT_LEAF_HELPERS(3, g_have_gb_pages)
+
+static inline pt_prot x86_leaf_pt_prot(pt_prot prot) {
+    if (prot.value & X86_PT_SMALL_PAT) {
+        prot.value &= ~X86_PT_SMALL_PAT;
+        prot.value |= X86_PT_HUGE_PAT;
+    }
+
+    return prot;
+}
+
+static inline void pt3_make_leaf(
+    struct pt3 *parent, phys_addr_t phys_addr, pt_prot prot
+)
+{
+    prot = x86_leaf_pt_prot(prot);
+    parent->value = phys_addr | prot.value | X86_PT_HUGE;
+}
+
+static inline void *pt3_to_virt(struct pt3 *pt)
+{
+    phys_addr_t phys_addr;
+    phys_addr_t mask = X86_PAGE_MASK;
+
+    if (pt->value & X86_PT_HUGE)
+        mask = X86_PT3_MASK;
+
+    phys_addr = pt->value & mask;
+    return phys_to_virt(phys_addr);
+}
+
 MAKE_X86_PT_HELPERS(2)
+
+#define ARCH_HAS_CUSTOM_PT2_LEAF
+MAKE_X86_PT_LEAF_HELPERS(2, true) // 2 MiB pages are always available
+
+static inline void pt2_make_leaf(
+    struct pt2 *parent, phys_addr_t phys_addr, pt_prot prot
+)
+{
+    prot = x86_leaf_pt_prot(prot);
+    parent->value = phys_addr | prot.value | X86_PT_HUGE;
+}
+
+static inline void *pt2_to_virt(struct pt2 *pt)
+{
+    phys_addr_t phys_addr;
+    phys_addr_t mask = X86_PAGE_MASK;
+
+    if (pt->value & X86_PT_HUGE)
+        mask = X86_PT2_MASK;
+
+    phys_addr = pt->value & mask;
+    return phys_to_virt(phys_addr);
+}
+
 MAKE_X86_PT_HELPERS(1)
+MAKE_X86_PT_TO_VIRT(1)
 
 static inline struct pt1 pt1_make_writeable(struct pt1 pt)
 {
