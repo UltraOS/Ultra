bbf7abf0c4fe09e8803227c9dc5b2a418d4b1727
Daniil Tatianin <99danilt@gmail.com>
2026-05-13 14:47:21 +0300

    kernel/console: make the consoles list global
    So that the logging facilities could access it.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/console.c
+++ kernel/console.c
@@ -1,12 +1,12 @@
 #include <console.h>
 
-static struct console *consoles;
+struct console *g_consoles;
 
 static bool console_registered(struct console *con)
 {
     struct console *this_con;
 
-    for (this_con = consoles; this_con; this_con = this_con->next) {
+    for (this_con = g_consoles; this_con; this_con = this_con->next) {
         if (this_con == con)
             return true;
     }
@@ -16,16 +16,16 @@ static bool console_registered(struct console *con)
 
 error_t register_console(struct console *con)
 {
-    if (!consoles) {
-        consoles = con;
+    if (!g_consoles) {
+        g_consoles = con;
         return EOK;
     }
 
     if (console_registered(con))
         return EBUSY;
 
-    con->next = consoles;
-    consoles = con;
+    con->next = g_consoles;
+    g_consoles = con;
     return EOK;
 }
 
@@ -34,7 +34,7 @@ error_t unregister_console(struct console *con)
     struct console *cur_con;
     struct console *prev_con = NULL;
 
-    for (cur_con = consoles; cur_con; cur_con = cur_con->next) {
+    for (cur_con = g_consoles; cur_con; cur_con = cur_con->next) {
         if (cur_con != con) {
             prev_con = cur_con;
             continue;
@@ -52,6 +52,6 @@ void console_write(const char *str, size_t count)
 {
     struct console *con;
 
-    for (con = consoles; con; con = con->next)
+    for (con = g_consoles; con; con = con->next)
         con->write(con, str, count);
 }
