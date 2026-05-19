3128b4762cac7c50b54bf7734eb86887404dcfbb
Daniil Tatianin <99danilt@gmail.com>
2026-05-19 13:33:22 +0300

    boot_alloc: add helpers for infalliable allocation
    Some of the early kernel allocations are critical and an OOM is not
    something we can recover from. Add a special helper for those so each
    such caller doesn't have to roll their own ad-hoc panic.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/boot/alloc.h
+++ kernel/include/boot/alloc.h
@@ -7,6 +7,9 @@ void boot_alloc_init(void);
 phys_addr_or_error_t boot_alloc(size_t num_pages);
 phys_addr_or_error_t boot_alloc_zeroed(size_t num_pages);
 
+void *boot_alloc_or_die(size_t num_pages, const char *why);
+void *boot_alloc_zeroed_or_die(size_t num_pages, const char *why);
+
 phys_addr_or_error_t boot_alloc_at(phys_addr_t addr, size_t num_pages);
 
 void boot_free(phys_addr_t address, size_t num_pages);
