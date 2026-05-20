76c172de9c81561c8b9764628f05444fdbe8f155
Daniil Tatianin <99danilt@gmail.com>
2026-05-20 13:10:05 +0300

    common/bit: add helpers for finding bits
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/common/bit.c
@@ -0,0 +1,47 @@
+#include <common/bit.h>
+
+reg_t find_next_bit_base(
+    const reg_t *bit_array, reg_t num_bits, reg_t start, bool inverted
+)
+{
+    reg_t first_unit, num_units, extra_bits, i;
+
+    if (start >= num_bits)
+        return num_bits;
+
+    first_unit = BIT_UNIT(start);
+    num_units = NUM_UNITS_FOR_BITS(num_bits);
+    extra_bits = num_bits % BITS_PER_UNIT;
+
+    for (i = first_unit; i < num_units; i++) {
+        reg_t unit, start_idx, bit;
+
+        unit = bit_array[i];
+
+        /*
+         * If looking for 0s, invert the unit before masking so out-of-bounds
+         * zeros don't accidentally become 1s and trigger false positives
+         */
+        if (inverted)
+            unit = ~unit;
+
+        // Mask out bits before the 'start' index in the first unit
+        if (i == first_unit) {
+            start_idx = BIT_INDEX(start);
+            if (start_idx > 0)
+                unit &= MAKE_BIT_MASK(BITS_PER_UNIT - 1, start_idx);
+        }
+
+        // Mask out bits beyond 'num_bits' in the last unit
+        if (i == num_units - 1 && extra_bits != 0)
+            unit &= MAKE_BIT_MASK(extra_bits - 1, 0);
+
+        // If there are any valid 1s left, find the lowest one
+        if (unit != 0) {
+            bit = BIT_UNIT_FIND_FIRST_SET(unit) - 1;
+            return (i * BITS_PER_UNIT) + bit;
+        }
+    }
+
+    return num_bits;
+}
