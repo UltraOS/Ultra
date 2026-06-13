2daf4f47f74d3d1d032be67886c7a6fbca2611dc
Daniil Tatianin <99danilt@gmail.com>
2026-06-13 08:39:32 -0400

    spinlock: fix a few bugs
    1. Fix ticket field order being mixed up between endian types
    2. Add a missing irq state parameter to try_lock
    3. Add a missing standalone SPINLOCK_INIT helper
    4. Use a relaxed load in try_lock
    5. Fix is_locked being incorrect in case of an overflow
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/spinlock.h
+++ kernel/include/spinlock.h
@@ -8,18 +8,19 @@ struct spinlock {
 
         struct {
         #ifdef ULTRA_ARCH_LITTLE_ENDIAN
-            u16 current_ticket;
             u16 last_freed_ticket;
+            u16 current_ticket;
         #else
-            u16 last_freed_ticket;
             u16 current_ticket;
+            u16 last_freed_ticket;
         #endif
 
         };
     };
 };
 
-#define DEFINE_SPINLOCK(name) struct spinlock name = { { .control = 0 } };
+#define SPINLOCK_INIT(name) (struct spinlock) { { .control = 0 } }
+#define DEFINE_SPINLOCK(name) struct spinlock name = SPINLOCK_INIT(name);
 
 static inline void spin_lock_init(struct spinlock *lock)
 {
@@ -30,7 +31,7 @@ void spin_lock(struct spinlock*);
 irq_state_t spin_lock_irq_save(struct spinlock*);
 
 bool spin_try_lock(struct spinlock*);
-bool spin_try_lock_irq_save(struct spinlock*);
+bool spin_try_lock_irq_save(struct spinlock*, irq_state_t *out_irq_state);
 
 void spin_unlock(struct spinlock*);
 void spin_unlock_irq_restore(struct spinlock*, irq_state_t);
