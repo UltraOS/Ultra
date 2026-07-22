8953b5710d8af30121ea44005e28c300b27eadab
Daniil Tatianin <99danilt@gmail.com>
2026-07-22 14:05:54 +0300

    memory: move alloc_behavior into a separate file
    This enum will be needed by all layers of allocators, including the
    buddy, as well as the virtual allocator, so let's split it out.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

---
+++ kernel/include/memory/alloc_behavior.h
@@ -0,0 +1,15 @@
+#pragma once
+
+enum alloc_behavior {
+    /*
+     * Generic kernel allocation, may sleep, use IO, reclaim,
+     * retry, or otherwise do things that may cause unpredictable
+     * delays.
+     */
+    ALLOC_GENERIC = 0 << 0,
+
+    /*
+     * Zero the allocated memory.
+     */
+    ALLOC_ZEROED = 1 << 0,
+};
