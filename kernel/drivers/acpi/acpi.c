425c5994e7753c97c93731e72136ada946f1e6d7
Daniil Tatianin <99danilt@gmail.com>
2026-08-18 15:49:18 +0300

    drivers: establish PLATFORM_INFO_AVAILABLE via callbacks
    Early ACPI table access is the promise nearly every consumer of this
    level actually depends on (MADT parsing, PM timer, HPET), so it
    becomes the level's establisher. SMBIOS discovery is an independent
    fellow traveler and becomes a PRE callback of the same level.
    
    Since the entry sequence no longer references either subsystem, the
    config-conditional declaration stubs in acpi.h and private/smbios.h
    are not needed anymore: when a subsystem is compiled out, its callback
    simply doesn't exist. Both headers end up empty, so delete them.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/drivers/acpi/acpi.c
+++ kernel/drivers/acpi/acpi.c
@@ -3,14 +3,16 @@
 
 #include <arch/constants.h>
 
-#include <acpi.h>
 #include <free_after_init.h>
+#include <init_level.h>
 
 #include <uacpi/uacpi.h>
 
 INIT_DATA ALIGN(ULTRA_ARCH_WIDTH) u8 early_table_buf[PAGE_SIZE];
 
-void INIT_CODE acpi_setup_tables(void)
+static error_t INIT_CODE acpi_setup_tables(void)
 {
     uacpi_setup_early_table_access(early_table_buf, sizeof(early_table_buf));
+    return EOK;
 }
+INIT_CALL_AT(PLATFORM_INFO_AVAILABLE, acpi_setup_tables);
