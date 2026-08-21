b967f38d68ea5e93b46ac69c66629efca76772cb
Daniil Tatianin <99danilt@gmail.com>
2026-08-21 20:09:47 +0300

    smp: track online CPUs as a cpu mask
    The vector allocator needs to know the set of CPUs it may place
    interrupts on, and a bare online counter cannot answer that. Introduce
    g_online_cpus and mark the boot CPU in it at registration time, the
    same place the online counter is set. AP bring-up will mark the rest
    as they come online.
    
    The counter stays as it is cheaper for the places that only need a
    number.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/include/smp.h
+++ kernel/include/smp.h
@@ -2,5 +2,11 @@
 
 #include <arch/smp.h>
 
+#include <cpu_mask.h>
+
 extern u32 g_num_present_cpus;
 extern u32 g_num_online_cpus;
+
+extern struct cpu_mask g_online_cpus;
+
+#define for_each_online_cpu(cpu) for_each_cpu_in(cpu, &g_online_cpus)
