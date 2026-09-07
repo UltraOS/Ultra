1b210651c28ec252e980720803bf7115722ce18a
Daniil Tatianin <99danilt@gmail.com>
2026-09-07 03:12:14 +0300

    error: drop the encoded error return types
    Nothing returns an error disguised as a pointer or an address any
    more, every interface reports an error_t and hands its result back
    through an out parameter. Remove the ptr_or_error_t and
    phys_addr_or_error_t types, their encode, decode and check macros,
    and the unused MAYBE_ERR. MAYBE_NERR stays for the printf family,
    whose negative int return is the C signature.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- tests/synthetic-include/common/error.h
+++ tests/synthetic-include/common/error.h
@@ -17,20 +17,8 @@ typedef int nerror_t;
 #define is_error(ret) unlikely((ret) != EOK)
 #define is_nerror(ret) unlikely((ret) < EOK)
 
-typedef void *ptr_or_error_t;
-typedef phys_addr_t phys_addr_or_error_t;
-
 /*
- * These macros are used as a hint to the reader that a function may also return
- * an error code even though its return type is not {n}error_t.
+ * A hint to the reader that a function may also return an error code even
+ * though its return type is not nerror_t.
  */
-#define MAYBE_ERR(value) value
 #define MAYBE_NERR(value) value
-
-#define encode_error_ptr(value) ((void*)((ptr_t)(value)))
-#define decode_error_ptr(value) ((error_t)((ptr_t)(value)))
-#define error_ptr(ret) unlikely(((ptr_t)(ret)) <= MAX_ERRNO)
-
-#define encode_error_phys_addr(value) ((phys_addr_t)(value))
-#define decode_error_phys_addr(value) ((error_t)(value))
-#define error_phys_addr(ret) unlikely(ret <= MAX_ERRNO)
