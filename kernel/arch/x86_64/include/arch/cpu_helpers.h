53d3204dbc602788f4e9a5dfb8688dff849327f7
Daniil Tatianin <99danilt@gmail.com>
2026-09-17 13:40:29 +0300

    arch/cpu_helpers: add arch_cpu_halt()
    A simple arch-specifc helper for gracefully halting the CPU.
    
    Signed-off-by: Daniil Tatianin <99danilt@gmail.com>

--- kernel/arch/x86_64/include/arch/cpu_helpers.h
+++ kernel/arch/x86_64/include/arch/cpu_helpers.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <common/attributes.h>
+
 static inline void arch_cpu_relax(void)
 {
     asm volatile("pause" ::: "memory");
+}
+
+static NORETURN inline void arch_cpu_halt(void)
+{
+    for (;;)
+        asm volatile("cli; hlt" ::: "memory");
 }
